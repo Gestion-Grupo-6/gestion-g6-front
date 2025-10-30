c9e688ceebe2dc75e8c8fbc68d997d3f542230c3
Candela Piccin <103950114+cpiccin@users.noreply.github.com>
2025-10-30 16:00:03 -0300

    add schema de reviews

---
+++ types/review.ts
@@ -0,0 +1,38 @@
+export type Ratings = {
+  cleanliness?: number
+  service?: number
+  location?: number
+}
+
+export type ReviewCreatePayload = {
+  ownerId: string
+  postId: string
+  comment?: string
+  ratings: Ratings
+  images?: string[]
+}
+
+export type ReviewUpdatePayload = {
+  ownerId: string
+  comment?: string
+  ratings: Ratings
+  images?: string[]
+}
+
+export type CommentResponse = {
+  id: string
+  type: "REVIEW" | "REPLY"
+  ownerId: string
+  postId: string
+  timestamp: string
+  comment?: string
+  likes: number
+  dislikes: number
+  deleted: boolean
+  edited: boolean
+  ratings?: Ratings
+  images?: string[]
+  replies?: CommentResponse[]
+}
+
+
