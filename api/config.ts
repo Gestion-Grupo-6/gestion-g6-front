74811d4a6beed653c05118c4870c311e25800498
astrocinco <santiagolanger@gmail.com>
2025-11-29 14:25:08 -0300

    Added: use cloud URL

--- api/config.ts
+++ api/config.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL || "http://localhost:8080"
+  process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL || "https://gestion-g6-back-971976303053.us-east4.run.app"
 
 const sanitizedBaseUrl = API_BASE_URL.replace(/\/$/, "")
 
